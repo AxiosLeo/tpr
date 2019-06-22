ebfefa518a6d7cdfcd07ebb324d2ccb99e65bc9c
axios <axiosleo@foxmail.com>
2019-06-22 14:29:21 +0800

    3.0-alpha

---
+++ global.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author  : axios
+ * @email   : axiosleo@foxmail.com
+ * @blog    : http://hanxv.cn
+ * @datetime: 2019-03-08 10:38
+ */
+
+const TPR_VERSION = "3.0.0";
+
+const TPR_BASE_NAMESPACE = "tpr";
