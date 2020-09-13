999fa7820576331cf4cc5c61a5d08aefbc80a67f
axios <axiosleo@foxmail.com>
2020-09-13 17:08:24 +0800

    deprecated nikic/fast-route

--- src/core/Dispatch.php
+++ src/core/Dispatch.php
@@ -5,11 +5,6 @@ declare(strict_types=1);
 namespace tpr\core;
 
 use Exception;
-use FastRoute\Dispatcher;
-use FastRoute\Dispatcher\GroupCountBased;
-use FastRoute\RouteCollector;
-use FastRoute\RouteParser\Std;
-use tpr\App;
 use tpr\Container;
 use tpr\Event;
 use tpr\exception\ClassNotExistException;
@@ -43,46 +38,13 @@ class Dispatch
      */
     public function run()
     {
-        $dispatcher = new GroupCountBased($this->getRoutes());
-        $request    = Container::request();
-        $routeInfo  = $dispatcher->dispatch($request->method(), $request->pathInfo());
-        $result     = null;
+        $request = Container::request();
+        $result  = null;
         Event::listen('app_cgi_dispatch', $routeInfo);
 
         try {
-            switch ($routeInfo[0]) {
-                case Dispatcher::NOT_FOUND:
-                    if (\tpr\Config::get('app.force_route', true)) {
-                        $pathInfo = $request->pathInfo() ?? '';
-                        $result   = $this->defaultRoute($pathInfo, $request->param());
-                    } else {
-                        Container::response()->response('Route Not Found', 404, []);
-                    }
-
-                    break;
-                case Dispatcher::METHOD_NOT_ALLOWED:
-                    Container::response()->response('Not Allowed Method', 405, []);
-
-                    break;
-                case Dispatcher::FOUND:
-                    $handler              = $routeInfo[1];
-                    $vars                 = $routeInfo[2];
-                    list($class, $action) = explode('::', $handler);
-                    $request->routeInfo($routeInfo);
-                    $rule = explode('\\', \tpr\Config::get('app.route_class_name', self::$defaultRouteClassName));
-                    $tmp  = explode('\\', $class);
-                    foreach ($rule as $i => $str) {
-                        if ('{module}' === $str) {
-                            $this->module = $tmp[$i];
-                        } elseif ('{controller}' === $str) {
-                            $this->controller = $tmp[$i];
-                        }
-                    }
-                    $this->action = $action;
-                    $result       = $this->exec($class, $action, $vars);
-
-                    break;
-            }
+            $pathInfo = $request->pathInfo();
+            $result   = $this->defaultRoute($pathInfo);
             $response = Container::response();
             $response->response($result);
         } catch (HttpResponseException $e) {
@@ -112,7 +74,7 @@ class Dispatch
         return $this->action;
     }
 
-    public function dispatch($module, $controller, $action, $params = [])
+    public function dispatch($module, $controller, $action)
     {
         $this->module     = $module;
         $this->controller = $controller;
@@ -125,53 +87,27 @@ class Dispatch
             'controller'    => ucfirst($this->controller),
         ]);
 
-        return $this->exec($class, $action, $params);
+        return $this->exec($class, $action);
     }
 
-    private function getRoutes()
+    private function defaultRoute($path_info)
     {
-        $route_data = null;
-        if (!App::debugMode()) {
-            $route_data = $this->cache($this->cache_file);
-        }
-
-        if (null === $route_data) {
-            $routeCollector = new RouteCollector(
-                new Std(),
-                new \FastRoute\DataGenerator\GroupCountBased()
-            );
-
-            $routes = \tpr\Config::get('routes', []);
-
-            foreach ($routes as $route_name => $route) {
-                $routeCollector->addRoute($route['method'], $route['rule'], $route['handler']);
-            }
-            $route_data = $routeCollector->getData();
-            if (!App::debugMode()) {
-                $this->cache($this->cache_file, $route_data);
-            }
-        }
-
-        return $route_data;
-    }
-
-    private function defaultRoute($path_info, $params)
-    {
-        $tmp  = explode('/', $path_info);
-        $path = [];
-        foreach ($tmp as $item) {
-            if (!empty($item)) {
-                array_push($path, $item);
+        if (null !== $path_info) {
+            $tmp  = explode('/', $path_info);
+            $path = [];
+            foreach ($tmp as $item) {
+                $p = $item ? $item : 'index';
+                array_push($path, $p);
             }
         }
         $module     = isset($path[0]) ? $path[0] : 'index';
         $controller = isset($path[1]) ? $path[1] : 'index';
         $action     = isset($path[2]) ? $path[2] : 'index';
 
-        return $this->dispatch($module, $controller, $action, $params);
+        return $this->dispatch($module, $controller, $action);
     }
 
-    private function exec($class, $action, $vars)
+    private function exec($class, $action, array $vars = [])
     {
         if (!class_exists($class)) {
             throw new ClassNotExistException('Class Not Exist : ' . $class, $class);
