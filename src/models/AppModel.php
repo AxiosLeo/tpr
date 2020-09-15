1f0d5d271430abcf64fba903fd16a65de90ddc8d
axios <axiosleo@foxmail.com>
2020-09-15 20:55:59 +0800

    add some properties for app config

--- src/models/AppModel.php
+++ src/models/AppModel.php
@@ -28,5 +28,45 @@ final class AppModel extends Model
      */
     public string $lang = 'zh-cn';
 
+    /**
+     * @var int global cache time for config&route data
+     */
+    public int $cache_time = 30;
+
+    /**
+     * @var string default content-type on cgi mode
+     */
+    public string $default_content_type_cgi = 'html';
+
+    /**
+     * @var string default content-type on api request
+     */
+    public string $default_content_type_ajax = 'json';
+
+    /**
+     * @var string default content-type on cli mode
+     */
+    public string $default_content_type_cli = 'text';
+
+    /**
+     * remove some header before send response.
+     *
+     * @var array|string[]
+     */
+    public array $remove_headers = [];
+
+    /**
+     * true : forces use routing mode.
+     */
+    public bool $force_route = false;
+
+    /**
+     * for dispatch route.
+     */
+    public string $dispatch_rule = '{app_namespace}\\{module}\\controller\\{controller}';
+
+    /**
+     * for ServerHandler custom config.
+     */
     public array $server_options = [];
 }
