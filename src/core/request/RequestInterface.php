778a8c8dd4ab73cfcad789389ba37040ce3f806d
axios <axiosleo@foxmail.com>
2020-10-02 09:13:57 +0800

    optimize request interface

--- src/core/request/RequestInterface.php
+++ src/core/request/RequestInterface.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace tpr\core\request;
 
+use tpr\models\RouteInfoModel;
+
 interface RequestInterface
 {
-    public function __call($name, $arguments);
+    public function uuid(): string;
 
-    public function url($is_whole = false);
+    public function url($is_whole = false): string;
 
-    public function contentType();
+    public function contentType(): string;
+
+    public function query(): string;
 
     public function param($name = null, $default = null);
-
-    public function input($array, $name = null, $default = null);
 
     public function get($name = null, $default = null);
 
@@ -22,25 +24,73 @@ interface RequestInterface
 
     public function put($name = null, $default = null);
 
-    public function delete($name = null, $default = null);
-
-    public function patch($name = null, $default = null);
-
     public function request($name = null, $default = null);
 
-    public function content();
+    public function content(): string;
 
     public function time($format = null, $micro = false);
 
     public function server($name = null);
 
-    public function routeInfo($routeInfo = null);
+    public function pathInfo(): string;
 
-    public function isHttps();
+    public function routeInfo(?RouteInfoModel $routeInfo = null);
 
-    public function scheme();
+    public function scheme(): string;
 
+    /**
+     * @param ?string $name
+     * @param ?string $default
+     *
+     * @return array|string
+     */
     public function header($name = null, $default = null);
 
+    /**
+     * get upload file.
+     *
+     * @param null|string $name
+     *
+     * @return null|\SplFileInfo|\SplFileInfo[]
+     */
     public function file($name = null);
+
+    public function method(): string;
+
+    /**
+     * get server software name.
+     *
+     * @example PHP 7.4.8 Development Server
+     */
+    public function env(): string;
+
+    public function host(): string;
+
+    public function port(): int;
+
+    public function indexFile(): string;
+
+    public function userAgent(): string;
+
+    public function accept(): string;
+
+    public function lang(): string;
+
+    public function encoding(): string;
+
+    public function isHttps(): bool;
+
+    public function isGet(): bool;
+
+    public function isPost(): bool;
+
+    public function isPut(): bool;
+
+    public function isDelete(): bool;
+
+    public function isHead(): bool;
+
+    public function isPatch(): bool;
+
+    public function isOptions(): bool;
 }
